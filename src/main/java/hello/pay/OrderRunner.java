07b50da91c3d57d835687d1a1ca957b0d54133d8
haleyji <hyeyounghaleyji@gmail.com>
2024-02-26 23:23:26 +0900

    initial commit

--- src/main/java/hello/pay/OrderRunner.java
+++ src/main/java/hello/pay/OrderRunner.java
@@ -11,6 +11,6 @@ public class OrderRunner implements ApplicationRunner {
     private final OrderService orderService;
     @Override
     public void run(ApplicationArguments args) throws Exception {
-        orderService.order(1000);
+//        orderService.order(1000); 주문하는 로직
     }
 }
